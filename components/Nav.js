3228317182736aebf4ba72cf94d0d0b4ee9b2c1d
Emaani-gh <abdulsalamseidu@protonmail.com>
2024-04-27 13:07:23 +0300

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <nav className="py-6 px-4">
+      <div className="logo font-bold text-xl cursor-pointer leter tracking-wider text-teal-500">
+        CryptoMani
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
